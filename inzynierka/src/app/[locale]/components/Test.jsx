9b0a6b10c3604979d7891486075902a37cb6cd76
BartoszNitschke <s25771@pjwstk.edu.pl>
2024-08-09 13:42:49 +0200

    test websocket 1.0

--- inzynierka/src/app/[locale]/components/Test.jsx
+++ inzynierka/src/app/[locale]/components/Test.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import SockJS from "sockjs-client";
+import { Client } from "@stomp/stompjs";
 
 const Test = () => {
+  const [stompClient, setStompClient] = useState(null);
+  const [message, setMessage] = useState("");
+  const [receivedMessage, setReceivedMessage] = useState("");
+
+  useEffect(() => {
+    // Tworzenie SockJS i STOMP clienta
+    const socket = new SockJS("https://localhost:8080/ws");
+    const client = new Client({
+      webSocketFactory: () => socket,
+      debug: function (str) {
+        console.log(str);
+      },
+      onConnect: () => {
+        console.log("Connected");
+        client.subscribe("/user/test12/test2", (message) => {
+          setReceivedMessage(message.body);
+        });
+      },
+      onStompError: (frame) => {
+        console.error("Broker reported error: " + frame.headers["message"]);
+        console.error("Additional details: " + frame.body);
+      },
+    });
+
+    client.activate();
+    setStompClient(client);
+
+    return () => {
+      if (client) {
+        client.deactivate();
+      }
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (stompClient && message.trim() !== "") {
+      stompClient.publish({
+        destination: "/app/test",
+        body: message,
+      });
+      setMessage("");
+    }
+  };
+
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <p className="text-white">siema</p>
+    <div>
+      <h1>WebSocket Client</h1>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Type your message here"
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <p>Received Message: {receivedMessage}</p>
     </div>
   );
 };
