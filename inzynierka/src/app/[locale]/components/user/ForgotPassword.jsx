678d94265bd1fd247604508c4cc6a501296c6cde
BartoszNitschke <s25771@pjwstk.edu.pl>
2025-01-20 20:14:05 +0100

    forgot password frontend

--- inzynierka/src/app/[locale]/components/user/ForgotPassword.jsx
+++ inzynierka/src/app/[locale]/components/user/ForgotPassword.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useMutation } from "react-query";
 import resetPassword from "../../api/user/resetPassword";
+import { IoRefreshOutline } from "react-icons/io5";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
@@ -23,22 +24,26 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="p-[120px] flex flex-col h-screen items-center bg-linen text-black">
-      <p>Forgot Password</p>
+    <div className="pt-[10%] flex flex-col w-full h-screen items-center bg-night text-white-smoke">
+      <p className="font-bangers text-[48px]">Forgot Password</p>
 
-      <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+      <form
+        className="mt-5 flex flex-col items-center font-chewy w-full"
+        onSubmit={handleSubmit}
+      >
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="mt-4 px-2 py-2 text-[22px] rounded-full font-semibold placeholder-oxford-blue focus:outline-none shadow-gray-800 shadow-lg"
+          className="w-[25%] px-3 py-2 text-[18px]  bg-transparent rounded-xl focus:outline-none text-white-smoke border-[1px] border-white-smoke"
         />
         <button
           type="submit"
-          className="mt-4 text-gray-100 bg-oxford-blue py-2 px-4 text-[22px] font-semibold rounded-full shadow-gray-900 shadow-lg hover:scale-105 transition-all duration-150"
+          className="flex items-center gap-x-1 hover:text-amber transition-all duration-150 cursor-pointer text-white-smoke mt-5 py-2 px-3"
         >
-          Reset Password
+          <IoRefreshOutline className="text-[28px]"></IoRefreshOutline>
+          <p className="text-[20px] ">Reset password</p>
         </button>
       </form>
     </div>
